0f912f8eefeb86aa51ff96acaf235642937c144e
ToQuery <toquery@qq.com>
2022-12-13 23:32:33 +0800

    1

---
+++ src/main/java/io/github/toquery/example/spring/data/jpa/ExampleSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.data.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/toquery/example/spring/data/jpa/ExampleSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.data.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
